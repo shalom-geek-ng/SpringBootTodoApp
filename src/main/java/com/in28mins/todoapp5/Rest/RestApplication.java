4aa8fcf21fc8051c25bda65e3ab4b57d199e4d57
shalom <oluwafemishalom3@gmail.com>
2024-01-01 11:32:18 +0100

    autowire todoservice to the Controller and use the method

--- src/main/java/com/in28mins/todoapp5/Rest/RestApplication.java
+++ src/main/java/com/in28mins/todoapp5/Rest/RestApplication.java
@@ -1,5 +1,9 @@
 package com.in28mins.todoapp5.Rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.in28mins.todoapp5.Todo.TodoService;
+import com.in28mins.todoapp5.Todo.todo;
+
 @Controller
 public class RestApplication {
+	@Autowired
+	private TodoService todoList;
+	
 //	@RequestMapping("/Loggin")
 //	@ResponseBody
 //	public String loggin() {
@@ -22,6 +32,8 @@ public class RestApplication {
 	@RequestMapping(value="/loggin", method=RequestMethod.POST)
 	public String todo(@RequestParam String name, @RequestParam String password,
 			ModelMap model) {
+		 List <todo> todo = todoList.todoMethod();
+		 model.put("todo", todo);
 		model.put("name", name);
 		model.put("password", password);
 		return "todo";
