3b3f267c88467e6085cba4c4ae9b00281e699b75
shalom <oluwafemishalom3@gmail.com>
2024-01-01 11:22:12 +0100

    update my todo service and add new id with ++count

--- src/main/java/com/in28mins/todoapp5/Todo/TodoService.java
+++ src/main/java/com/in28mins/todoapp5/Todo/TodoService.java
@@ -1,5 +1,21 @@
 package com.in28mins.todoapp5.Todo;
 
-public class TodoService {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+	public static List<todo> mytodo = new ArrayList<todo>();
+	private static int count=0;
+	static {
+	mytodo.add(new todo(++count,"Shalom","Code with springboot",LocalDate.now(),true));
+	mytodo.add(new todo(++count,"Mason","Read todo",LocalDate.now().plusDays(2),false));
+	}
+	
+	public List<todo> todoMethod(){
+		return mytodo;
+	}
 }
--- src/main/java/com/in28mins/todoapp5/Todo/todo.java
+++ src/main/java/com/in28mins/todoapp5/Todo/todo.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class todo {
+	private int id;
 	private String name;
 	private String description;
 	private LocalDate date;
@@ -16,53 +17,71 @@ public class todo {
 		
 	}
 
-	public todo(String name, String description, LocalDate date, Boolean done) {
-		super();
-		this.name = name;
-		this.description = description;
-		this.date = date;
-		this.done = done;
-	}
 
-//	Getters
-	public String getName() {
-		return name;
-	}
 
-	public String getDescription() {
-		return description;
-	}
+public todo(int id, String name, String description, LocalDate date, Boolean done) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.description = description;
+	this.date = date;
+	this.done = done;
+}
 
-	public LocalDate getDate() {
-		return date;
-	}
+// Getters
 
-	public Boolean getDone() {
-		return done;
-	}
-//	Setters
+public int getId() {
+	return id;
+}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+public String getName() {
+	return name;
+}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+public String getDescription() {
+	return description;
+}
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+public LocalDate getDate() {
+	return date;
+}
 
-	public void setDone(Boolean done) {
-		this.done = done;
-	}
+public Boolean getDone() {
+	return done;
+}
 
-//	ToString
-	@Override
-	public String toString() {
-		return "todo [name=" + name + ", description=" + description + ", date=" + date + ", done=" + done + "]";
-	}
+//Setters
+public void setId(int id) {
+	this.id = id;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public void setDate(LocalDate date) {
+	this.date = date;
+}
+
+public void setDone(Boolean done) {
+	this.done = done;
+}
+
+
+
+@Override
+public String toString() {
+	return "todo [id=" + id + ", name=" + name + ", description=" + description + ", date=" + date + ", done=" + done
+			+ "]";
+}
+
+
+
+	
 	
 	
 	
